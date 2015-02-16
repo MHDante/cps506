2baf36e529e1e86052eef8a44bbe29c0916e201b
Dante Camarena <mhdcj@hotmail.com>
2015-02-16 04:35:32 -0800

    Functions Written!

--- a1/Assign1.hs
+++ a1/Assign1.hs
@@ -1,4 +1,9 @@
-module Assign1(ass) where
-    trifecta :: (a -> b) -> (b -> c) -> (c -> d) -> a -> d
+module Assign1 where
+    trifecta :: (a -> b) -> (b -> c) -> (c -> d) -> (a -> d)
+    trifecta a b c d = c$b$a$d
+    
     mapCF :: a -> (a -> b -> c) -> [b] -> [c]
+    mapCF a b c = map f c where f = b a
+    
     allPairFunc :: [a -> b -> Bool] -> [(a,b)] -> Bool
+    allPairFunc (h1:t1) (h2:t2) = ((h1 (fst h2)) (snd h2)) && allPairFunc t1 t2
