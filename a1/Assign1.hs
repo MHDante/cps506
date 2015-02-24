24ab229d79e790828e0dcc9fb59c9eb94980918a
Dante Camarena <mhdcj@hotmail.com>
2015-02-24 11:59:06 -0800

    Missing Test Cases

--- a1/Assign1.hs
+++ a1/Assign1.hs
@@ -1,43 +1,73 @@
 module Assign1 where
     import Data.Char
 
-    trifecta :: (a -> b) -> (b -> c) -> (c -> d) -> (a -> d)
+    trifecta :: (a -> b) -> (b -> c) -> (c -> d) -> a -> d
     trifecta a b c d = c$b$a$d
     
     mapCF :: a -> (a -> b -> c) -> [b] -> [c]
-    mapCF a b c = map f c where f = b a
+    mapCF a b  = map f where f = b a
     
     allPairFunc :: [a -> b -> Bool] -> [(a,b)] -> Bool
     allPairFunc (h1:t1) (h2:t2) = ((h1 (fst h2)) (snd h2)) && allPairFunc t1 t2
     allPairFunc [] [] = True
-    allPairFunc (h:t) [] = False
-    allPairFunc [] (h:t) = False
-    
+    allPairFunc (_:_) [] = False
+    allPairFunc [] (_:_) = False
+
+    divBy :: Int -> Int -> Bool
     divBy x y = (mod y x) == 0
+    
+    greaterThan :: Int -> Int -> Bool
     greaterThan x y = x>y
+    
+    equal :: Int -> Int -> Bool
     equal x y = x==y
     
+    fns1 :: [Int -> Int -> Bool]
     fns1 = [divBy, greaterThan, equal]
+    
+    pairs1 :: [(Int, Int)]
     pairs1 = [(2,4),(5,3), (2,2)]
+    
+    tst1 :: Bool
     tst1 = allPairFunc fns1 pairs1
 
+    
+    fns2 :: [Int -> Int -> Bool]
     fns2 = [divBy, greaterThan, equal]
+    
+    pairs2 :: [(Int, Int)]
     pairs2 = [(2,4),(3,3), (2,2)]
+    
+    tst2 :: Bool
     tst2 = allPairFunc fns2 pairs2
 
+    fns3 :: [Int -> Int -> Bool]
     fns3 = [divBy, greaterThan, equal]
+    
+    pairs3 :: [(Integer, Integer)]
     pairs3 = [(2,4),(3,3), (2,2), (1337,2)]
+    
+    tst3 :: Bool
     tst3 = allPairFunc fns2 pairs2
+    
+    allPairFuncCorrect :: Bool
     allPairFuncCorrect = (tst1 == True && tst2 == False && tst3 == False)
     
+    tst4 :: [Bool]
     tst4 = mapCF 2 divBy [3,4,5,6,7]
+    tst5 :: [Bool]
     tst5 = mapCF 5 greaterThan [3,4,5,6,7]
+    mapCFCorrect :: Bool
     mapCFCorrect = (tst4 == [False,True,False,True,False]) && (tst5 == [True,True,False,False,False])
     
+    myToLower :: Char -> Char
     myToLower x = trifecta ord (+32) chr x
+    tst6 :: Char
     tst6 = myToLower 'S'
+    trifectaCorrect :: Bool
     trifectaCorrect = tst6 == 's'
     
+    main :: IO ()
     main = do        
         if mapCFCorrect && trifectaCorrect && allPairFuncCorrect
             then print("Assignment Complete!")
--- a2/Assign2.hs
+++ a2/Assign2.hs
@@ -1,19 +1,23 @@
 module Assign2 where
-
-    data Sink = Yes|
+    --import Test.HUnit
+    --import Test.QuickCheck
+    
+    data Sink t = Yes|
                 No|
                 AnInteger Int|
                 ADouble Double|
-                List [Sink] |
-                AString [Char]
+                List [Sink t] |
+                AString [Char] |
+                Other t
             
-    instance Show Sink where
+    instance Show (Sink t) where
         show Yes = "True"
         show No = "False"
         show (AnInteger i) = show i
         show (ADouble d) = show d
         show (List l) = show l
         show (AString s) = show s
+        show (Other _) = "This sink is Generic"
 
     class Extract t where
         asBool:: t->Bool
@@ -39,7 +43,7 @@ module Assign2 where
         asInteger d = truncate d
         asDouble = id
 
-    instance Extract Sink where
+    instance Extract (Sink t) where
         asBool Yes = True
         asBool No = False
         asBool (AnInteger i) = asBool i
@@ -47,12 +51,15 @@ module Assign2 where
         asBool (List l) = (length l) > 0 
         asBool (AString s) | (elem s ["True","true","yes","Yes"]) = True
                            | otherwise = False
+        asBool (Other _) = error "Cannot Convert Generic Sink to Bool"
+        
         asInteger Yes = 1
         asInteger No = 0
         asInteger (AnInteger i) = asInteger i
         asInteger (ADouble d) = asInteger d
         asInteger (List l) = length l
         asInteger (AString s) = read s :: Int
+        asInteger (Other _) = error "Cannot Convert Generic Sink to Integer"
 
         asDouble Yes = 1.0
         asDouble No = 0.0
@@ -60,7 +67,54 @@ module Assign2 where
         asDouble (ADouble d) = asDouble d
         asDouble (List l) = asDouble $ length l
         asDouble (AString s) = read s :: Double
+        asDouble (Other _) = error "Cannot Convert Generic Sink to Double"
 
-    
+    instance Num (Sink t) where
+        Yes + Yes = Yes
+        No + No = No
+        No + Yes = Yes
+        Yes + No = Yes
+        (AnInteger a) + x = AnInteger (a + (asInteger x))
+        x + (AnInteger a) = AnInteger (a + (asInteger x))
+        (ADouble a) + x = ADouble (a + (asDouble x))
+        x + (ADouble a) = ADouble (a + (asDouble x))
+        (List a) + (List b) = List (a++b)
+        (AString a) + (AString b) = AString(a++b)
+        _ + _ = error "Operation Not Supported"
+
+        Yes * Yes = Yes
+        No * No = No
+        No * Yes = No
+        Yes * No = No
+        (AnInteger a) * x = AnInteger (a * (asInteger x))
+        x * (AnInteger a) = AnInteger (a * (asInteger x))
+        (ADouble a) * x = ADouble (a * (asDouble x))
+        x * (ADouble a) = ADouble (a * (asDouble x))
+        _ * _ = error "Operation Not Supported"
+
+        negate Yes = No
+        negate No = Yes
+        negate (AnInteger a) = AnInteger $ negate a
+        negate (ADouble a) = ADouble $ negate a
+        negate (List a) = List $ reverse a
+        negate (AString a) = AString $ reverse a
+        negate _ = error "Operation Not Supported"
+
+        abs Yes = Yes
+        abs No = No
+        abs (AnInteger a) = AnInteger $ abs a
+        abs (ADouble a) = ADouble $ abs a
+        abs _ = error "Operation Not Supported"
+
+        fromInteger x = AnInteger $ fromIntegral x
+
+        signum Yes = No
+        signum No = Yes
+        signum (AnInteger a) = AnInteger $ signum a
+        signum (ADouble a) = ADouble $ signum a
+        signum (List a) = AnInteger $ signum $ length a
+        signum (AString a) = AnInteger $ signum $ length a
+        signum _ = error "Operation Not Supported"
+        
     main :: IO ()
     main = print (asBool $ AString "no")
