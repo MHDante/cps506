69f6f8e59cc5a6f91d6520923307ecde40d7713a
MHDante <mhdcj@hotmail.com>
2015-03-27 22:32:17 -0400

    a4 done

--- a4/Assign4.clj
+++ a4/Assign4.clj
@@ -7,12 +7,12 @@
 (defmethod abrv :default [] (error "Symbol not accepted Assign4/abrv."))
 
 (defprotocol Show
- (show [x] "UserReadable string representation")
- )
+  (show [x] "UserReadable string representation")
+  )
 
 (extend-type Object
- Show (show [x] (str x))
- )
+  Show (show [x] (str x))
+  )
 
 (declare select analHelper digitToInt parse updateState)
 
@@ -20,6 +20,7 @@
   ([] nil)
   ([& _] nil))
 
+(defn digitToInt [chr] (- (int chr) (int \0)))
 ;;----------COLOR
 (def colors #{:Black :White})
 
@@ -27,11 +28,11 @@
 (defmethod abrv :White [_] "W")
 
 (defn negate [sym] (cond
-                    (= sym :Black) :White
-                    (= sym :White) :Black
-                    :else (error "Symbol not accepted Assign4/negate.")
-                    )
- )
+                     (= sym :Black) :White
+                     (= sym :White) :Black
+                     :else (error "Symbol not accepted Assign4/negate.")
+                     )
+  )
 ;;----------END COLOR
 (def ranks #{:King :Queen :Rook :Knight :Bishop :Pawn :Empty})
 (defmethod abrv :King [_] "K")
@@ -43,30 +44,30 @@
 (defmethod abrv :Empty [_] "")
 ;;----------PIECE
 (defrecord Piece [rank color]
- Show
- (show [_] (cond
-            (= rank :Knight) (str (abrv color) (abrv rank))
-            (= rank :Empty) "   "
-            :else (str (abrv color) (abrv rank) " ")
-            )
+  Show
+  (show [_] (cond
+              (= rank :Knight) (str (abrv color) (abrv rank))
+              (= rank :Empty) "   "
+              :else (str (abrv color) (abrv rank) " ")
+              )
+    )
   )
- )
 
 ;;eatOrOccupy :: Board -> Color -> Coord -> Maybe Action
 
 (defn eatOrOccupy [board col crd] (let [pce (select board crd)]
-                                   (cond
-                                    (nil? pce) nil
-                                    (= (:rank pce) :Empty) '(:Occupy crd)
-                                    (not= (:color pce) col) '(:Eat crd)
-                                    :else nil
-                                    )
-                                   )
- )
-(defn takeIfOccupied [board col crd] (let [res (eatOrOccupy board col crd)]
-                                      (if (= res '(:Occupy crd)) nil res)
+                                    (cond
+                                      (nil? pce) nil
+                                      (= (:rank pce) :Empty) [:Occupy crd]
+                                      (not= (:color pce) col) [:Eat crd]
+                                      :else nil
                                       )
- )
+                                    )
+  )
+(defn takeIfOccupied [board col crd] (do (println (str "This one" crd))(let [res (eatOrOccupy board col crd)]
+                                       (if (= res [:Occupy crd]) nil res)
+                                       ))
+  )
 
 ;;takeIfOccupied :: Board -> Color -> Coord -> Maybe Action
 
@@ -91,65 +92,58 @@
     (and (= c \space) (= d e nil)) (getAnalyzer pce (str a b " 8"))
     (and (= d \space) (= e nil)) (getAnalyzer pce (str a b c))
     (= b c d e nil) (getAnalyzer pce (str a " 8"))
-    (= c d e nil) (getAnalyzer pce (str b e " 8"))
-    (and (= pce (Piece. :Pawn :Black)) (= a \S) (= b \space) (= d e nil))
-    (fn blackPawnMove [board [x y]] (if (not (contains? "12345678" c))
-                        nil
-                        (if (and
-                              (not= c \1)
-                              (= y 6)
-                              (= (:rank (select board [x (- y 1)])) (:rank (select board [x (- y 2)])) :Empty)
-                              )
-                          '(:Occupy [x (- y 2)])
-                          (if (= (:rank (select board [x (- y 1)])) :Empty)
-                            '(:Occupy [x (- y 1)])
-                            nil
-                            )
+    (= c d e nil) (getAnalyzer pce (str a b " 8"))
+    (= (:rank pce) :Pawn)
+    (cond
+      (and (= b \space) (= d e nil))
+      (do (println (str a ":" b ":"c":"d":"e))(fn pawnMove [board [x y]]
+        (if (or (not (contains? (set "12345678") c)) (not= (if (= (:color pce) :Black) \S \N) a))
+          nil
+          (if (and
+                (not= c \1)
+                (= y (if (= (:color pce) :Black) 6 1))
+                (=
+                  (:rank (select board [x (if (= (:color pce) :Black) (- y 1) (+ y 1))]))
+                  (:rank (select board [x (if (= (:color pce) :Black) (- y 2) (+ y 2))]))
+                  :Empty
+                  )
+                )
+            [:Occupy [x (if (= (:color pce) :Black) (- y 2) (+ y 2))]]
+            (if (= (:rank (select board [x (if (= (:color pce) :Black) (- y 1) (+ y 1))])))
+                   [:Occupy [x (if (= (:color pce) :Black) (- y 1) (+ y 1))]]
+                   nil
+                   )
+              )
+            )
+          ))
+      (= c \space)
+      (fn pawnEat [board [x y]]
+        (takeIfOccupied board (:color pce)
+                        (cond
+                          (and (= (:color pce) :Black) (= (str a b) "SE")) [(+ x 1) (- y 1)]
+                          (and (= (:color pce) :Black) (= (str a b) "SW")) [(- x 1) (- y 1)]
+                          (and (= (:color pce) :White) (= (str a b) "NE")) [(+ x 1) (+ y 1)]
+                          (and (= (:color pce) :White) (= (str a b) "NW")) [(- x 1) (+ y 1)]
+                          :else [-1 -1]
                           )
                         )
-      )
-    (and (= pce (Piece. :Pawn :White)) (= a \N) (= b \space) (= d e nil))
-    (fn whitePawnMove [board [x y]] (if (not (contains? "12345678" c))
-                        nil
-                        (if (and
-                              (not= c \1)
-                              (= y 1)
-                              (= (:rank (select board [x (+ y 1)])) (:rank (select board [x (+ y 2)])) :Empty)
-                              )
-                          '(:Occupy [x (+ y 2)])
-                          (if (= (:rank (select board [x (+ y 1)])) :Empty)
-                            '(:Occupy [x (+ y 1)])
-                            nil
-                            )
-                          )
-                        )
-      )
-    (and (= (:rank pce) :Pawn) (= c \space) (contains? "12345678" d))
-    (fn pawnEat [board [x y]]
-      (takeIfOccupied board (:color pce)
-                      (cond
-                        (and (= (:color pce) :White) (= (str a b) "SE")) [(- x 1) (- y 1)]
-                        (and (= (:color pce) :White) (= (str a b) "SW")) [(+ x 1) (- y 1)]
-                        (and (= (:color pce) :Black) (= (str a b) "NE")) [(- x 1) (+ y 1)]
-                        (and (= (:color pce) :Black) (= (str a b) "NW")) [(+ x 1) (+ y 1)]
-                        :else [-1 -1]
-                        )
-                      )
+        )
+      :else nullify
       )
     (
       = (:rank pce) :Knight)
     (fn knightMove [board [x y]] (eatOrOccupy board (:color pce) (cond
-                                                        (= (str a b) "NNE") [(+ x 1) (+ y 2)]
-                                                        (= (str a b) "NNW") [(- x 1) (+ y 2)]
-                                                        (= (str a b) "EEN") [(+ x 2) (+ y 1)]
-                                                        (= (str a b) "EES") [(+ x 2) (- y 1)]
-                                                        (= (str a b) "SSE") [(+ x 1) (- y 2)]
-                                                        (= (str a b) "SSW") [(- x 1) (- y 2)]
-                                                        (= (str a b) "WWN") [(- x 2) (+ y 1)]
-                                                        (= (str a b) "WWS") [(- x 2) (- y 1)]
-                                                        :else [-1 -1]
-                                                        )
-                                   )
+                                                                   (= (str a b c) "NNE") [(+ x 1) (+ y 2)]
+                                                                   (= (str a b c) "NNW") [(- x 1) (+ y 2)]
+                                                                   (= (str a b c) "EEN") [(+ x 2) (+ y 1)]
+                                                                   (= (str a b c) "EES") [(+ x 2) (- y 1)]
+                                                                   (= (str a b c) "SSE") [(+ x 1) (- y 2)]
+                                                                   (= (str a b c) "SSW") [(- x 1) (- y 2)]
+                                                                   (= (str a b c) "WWN") [(- x 2) (+ y 1)]
+                                                                   (= (str a b c) "WWS") [(- x 2) (- y 1)]
+                                                                   :else (do (println "happen") [-1 -1])
+                                                                   )
+                                              )
       )
     (
       and (= (:rank pce) :King) (= b \space) (= d e nil))
@@ -159,13 +153,13 @@
     (and (= (:rank pce) :Rook) (= c \space) (= e nil)) nullify
     (and (= (:rank pce) :Bishop) (= b \space) (= d e nil)) nullify
     (and (= b \space) (= d e nil))
-    (if-let [d (get dirTable (str a))]
-      (analHelper d (digitToInt c) 1 (:color pce))
+    (if-let [dir (get dirTable (str a))]
+      (do (println dir) (analHelper dir (digitToInt c) 1 (:color pce)))
       nullify
       )
     (and (= c \space) (= e nil))
-    (if-let [d (get dirTable (str a b))]
-      (analHelper d (digitToInt d) 1 (:color pce))
+    (if-let [dir (get dirTable (str a b))]
+      (analHelper dir (digitToInt d) 1 (:color pce))
       nullify
       )
     :else nullify
@@ -173,7 +167,7 @@
   )
 (defn analHelper [[dx dy] limit i c]
   (fn ahh [board [x y]]
-    (let [coords [(* (+ x i) dx) (* (+ y i) dy)]]
+    (let [coords [(+ x (* i dx)) (+ y (* i dy))]]
       (cond
         (> i limit) nil
         (not= (:rank (select board coords)) :Empty) (eatOrOccupy board c coords)
@@ -187,13 +181,13 @@
 ;;----------BOARD
 (defn select [b [x y]] (if
                          (or
-                           (> y (- (count b) 1))
+                           (>= y 8)
                            (< y 0)
                            (< x 0)
-                           (> x (count (first b)))
+                           (>= x 8)
                            )
-                         nil
-                         (get (get b y) x)
+                         (do (println (str "Out of Bounds" x " " y)) nil)
+                         (do (println (str "In Bounds" x " " y))(nth (nth b y) x))
                          )
   )
 
@@ -201,13 +195,13 @@
 (def letters (apply str (map #(str "   " %) "abcdefgh")))
 
 
-(defrecord BoardState [board blackCaps whiteCaps Turn message]
+(defrecord BoardState [board blackCaps whiteCaps turn message]
   Show
   (show [_] (letfn [
                     (msg [i] (cond
-                               (= i 8) (str "  White Captures: " whiteCaps \newline)
-                               (= i 6) (str "  Black Captures: " blackCaps \newline)
-                               (= i 4) (str "  Current Turn: " Turn \newline)
+                               (= i 8) (str "  White Captures: " (apply str (map show whiteCaps)) \newline)
+                               (= i 6) (str "  Black Captures: " (apply str (map show blackCaps)) \newline)
+                               (= i 4) (str "  Current Turn: " turn \newline)
                                (= i 2) (str "  " message \newline)
                                :else \newline
                                )
@@ -221,7 +215,7 @@
                     (showHelper [[h & t :as xs] i]
                                 (if (empty? xs)
                                   ""
-                                  (str (showHelper t (+ i 1)) (count xs) (showRow h) divider (msg i))
+                                  (str (showHelper t (+ i 1)) i (showRow h) divider (msg i))
                                   )
                                 )
                     ]
@@ -231,68 +225,71 @@
   )
 ;;----------END BOARD
 
-(defn applyMove [{:keys [board b w turn _] :as bs} mv]
-  (let [[pce coord anlz] (parse board mv)] ;(Empty, (0,0), defAnalyzer)
-    (cond
-      (= (:rank pce) :Empty) (BoardState. board b w turn "Could Not Select Piece")
-      (not= (:color pce) turn) (BoardState. board b w turn (str "Not your Turn! " show pce))
-      :else (let [act (anlz board coord)]
-                  (if (nil? act)
-                    (BoardState. board b w turn "Could Not Perform Move")
-                    (updateState bs act coord)
+(defn applyMove [{:keys [board blackCaps whiteCaps turn message] :as bs} mv]
+  (if-let [res (parse board mv)]
+    (let [[pce coord anlz ] res] ;(Empty, (0,0), defAnalyzer)
+      (cond
+        (= (:rank pce) :Empty) (BoardState. board blackCaps whiteCaps turn "Could Not Select Piece")
+        (not= (:color pce) turn) (BoardState. board blackCaps whiteCaps turn (str "Not your Turn! " (:keys bs)))
+        :else (let [act (anlz board coord)]
+                (if (do (println act) (nil? act))
+                  (BoardState. board blackCaps whiteCaps turn "Could Not Perform Move")
+                  (updateState bs act coord)
                   )
-             )
+                )
+        )
       )
+    (BoardState. board blackCaps whiteCaps turn "Could Not Select Piece...")
+    )
   )
-)
 
-(defn updateState [{:keys [board b w turn _]} [action [x1 y1]] [x0 y0]]
+(defn updateState [{:keys [board blackCaps whiteCaps turn message]} [action [x1 y1]] [x0 y0]]
   (let [pce (select board [x0 y0])]
     (letfn [
             (updateRow [[h2 & t2 :as xs2] x3 y3]
                        (if (empty? xs2)
                          []
-                         (conj (updateRow t2 (+ x3 1) y3)
-                               (cond
-                                 (and (= x1 x3) (= y1 y3)) pce
-                                 (and (= x0 x3) (= y0 y3)) (Piece. :Empty nil)
+                         (cons (cond
+                                 (and (= x1 x3) (= y1 y3)) (do (println (str "Move!" x3 " " y3 " " pce)) pce)
+                                 (and (= x0 x3) (= y0 y3)) (do (println (str "Empty!" x1 " " y1)) (Piece. :Empty nil))
                                  :else h2
                                  )
+                               (updateRow t2 (+ x3 1) y3)
                                )
                          )
                        )
             (updateBoard [[h1 & t1 :as xs1] x2 y2]
                          (if (empty? xs1)
                            []
-                           (conj (updateRow h1 x2 y2) (updateBoard t1 x2 (+ y2 1)))
+                           (cons (updateRow h1 x2 y2) (updateBoard t1 x2 (+ y2 1)))
                            )
                          )
             ]
       (cond
         (= action :Eat) (BoardState.
                           (updateBoard board 0 0)
-                          (if (= turn :Black) b (conj b pce))
-                          (if (= turn :White) w (conj w pce))
+                          (if (= turn :Black) blackCaps (conj blackCaps pce))
+                          (if (= turn :White) whiteCaps (conj whiteCaps pce))
                           (negate turn)
                           (str (show pce) " ate " (show (select board [x1 y1])))
                           )
-        (= action :Occupy) (BoardState. (updateBoard board 0 0) b w (negate turn) (str (show pce) " moved to " [x1 y1]))
+        (= action :Occupy) (BoardState. (updateBoard board 0 0) blackCaps whiteCaps (negate turn) (str (show pce) " moved"))
         )
       )
     )
   )
 
 (defn parse [board [a n s & xs]]
-  (let [coord [(- (int a) (int \a)) (- 8 (- (int n) (int \0)))]
+  (let [coord [(- (int a) (int \a)) (dec (digitToInt n))]
         pce (select board coord)]
     (if (or
           (nil? pce)
           (not= s \space)
-          (not (contains? "abcdefgh" a))
-          (not (contains? "12345678" n))
+          (not (contains? (set "abcdefgh") a))
+          (not (contains? (set "12345678") n))
           (empty? xs)
           )
-      nil
+      (do (println coord) nil)
       [pce, coord (getAnalyzer pce xs)]
       )
     )
